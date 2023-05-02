286b5a514de396de2d9787ef351937bf8a00493d
Afrin Akter <akterafrin3600@gmail.com>
2023-05-03 01:13:02 +0600

    get chef data in json

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const chef = require('./Data/chef.json');
 
 
 app.get('/', (req, res) =>{
     res.send('Food is running')
 });
+
+app.get('/chef',(req, res) =>{
+    res.send(chef)
+})
 
 app.listen(port, () => {
     console.log(`food API is running on port: ${port}`)
