c02353b12168933556cfd4b9e01c417d85204b6e
Afrin Akter <akterafrin3600@gmail.com>
2023-05-03 01:28:33 +0600

    added cors to fetch data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 const chef = require('./Data/chef.json');
 
-
+app.use(cors())
 app.get('/', (req, res) =>{
     res.send('Food is running')
 });
